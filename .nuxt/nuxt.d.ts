5f1c1be0ee7c30073df9831d6f7240cb3365ffd6
dev-zak <zakiagokoli036@gmail.com>
2026-01-14 13:27:08 +0100

    initiation de dossiers

---
+++ .nuxt/nuxt.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="@nuxt/devtools" />
+/// <reference types="@nuxt/telemetry" />
+/// <reference path="types/builder-env.d.ts" />
+/// <reference path="types/plugins.d.ts" />
+/// <reference path="types/build.d.ts" />
+/// <reference path="types/app.config.d.ts" />
+/// <reference path="types/runtime-config.d.ts" />
+/// <reference types="nuxt/app" />
+/// <reference types="C:/Users/LENOVO/Desktop/Nuxt JS/Blog-test/node_modules/.pnpm/@nuxt+nitro-server@4.2.2_db_6d57811d8bd5a36a956ffdac6aa545f3/node_modules/@nuxt/nitro-server/dist/index.mjs" />
+/// <reference path="types/middleware.d.ts" />
+/// <reference path="types/components.d.ts" />
+/// <reference path="imports.d.ts" />
+/// <reference path="types/imports.d.ts" />
+/// <reference path="schema/nuxt.schema.d.ts" />
+/// <reference path="types/nitro.d.ts" />
+
+export {}
---
+++ .nuxt/nuxt.node.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="@nuxt/devtools" />
+/// <reference types="@nuxt/telemetry" />
+/// <reference path="types/modules.d.ts" />
+/// <reference path="types/runtime-config.d.ts" />
+/// <reference path="types/app.config.d.ts" />
+/// <reference types="nuxt" />
+/// <reference types="../node_modules/.pnpm/@nuxt+vite-builder@4.2.2_ma_a22f546f4d0c2e54b92f87d88fac443d/node_modules/@nuxt/vite-builder/dist/index.mjs" />
+/// <reference types="C:/Users/LENOVO/Desktop/Nuxt JS/Blog-test/node_modules/.pnpm/@nuxt+nitro-server@4.2.2_db_6d57811d8bd5a36a956ffdac6aa545f3/node_modules/@nuxt/nitro-server/dist/index.mjs" />
+/// <reference path="types/middleware.d.ts" />
+/// <reference path="schema/nuxt.schema.d.ts" />
+
+export {}
